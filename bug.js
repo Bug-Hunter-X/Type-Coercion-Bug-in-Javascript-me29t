60164cfbcb421cc22d45cad8023fbe600bb1101b
BugHunter-X <askzenai@gmail.com>
2025-02-04 17:18:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a + b; 
+}
+
+function subtract(a, b) {
+  return a - b; 
+}
+
+console.log(add(5,3)); // Output: 8
+console.log(subtract(5,3)); // Output: 2
+
+//This is wrong way to use the function. It will cause unexpected error.
+console.log(add(5,"3")); //Output: 53
